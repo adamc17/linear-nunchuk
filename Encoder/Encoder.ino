3a0cd7b9b7454d8e00c7cebe6580b267a94d58be
unknown <adamc17@NIOBE.vrac.iastate.edu>
2012-06-22 10:49:32 -0500

    Add initial version, with overzealous attempts to make a wiimote class

---
+++ Encoder/Encoder.ino
@@ -0,0 +1,133 @@
+#include <PinChangeInt.h>
+#include <Wire.h>
+#include "FakeWiimoteExtension.h"
+
+
+FakeNunchuk nunchuk;
+
+//Declares Output Pins
+enum {
+	pinI = 3,
+	pinA = 4,
+	pinB = 5
+
+};
+
+
+/* The pattern goes like this, with values (A, B)
+ (0,0), (1,0), (1,1), (0,1)
+
+ Keep track of what it was last time, and do something to counter
+ (add one, subtract one, or do nothing if the same which should never happen)
+ */
+
+
+volatile int previous;
+volatile bool lastA;
+volatile bool lastB;
+volatile int counter = 0;
+
+inline int encodeSequencePosition(bool Aval, bool Bval) {
+	if (Bval == 0) {
+		if (Aval == 0) {
+			return 0;
+		} else {
+			return 1;
+		}
+	} else { // bval is 1
+		if (Aval == 1) {
+			return 2;
+		} else {
+			return 3;
+		}
+	}
+}
+
+
+void setupEncoder() {
+	// input pins
+	pinMode(pinA, INPUT);
+	pinMode(pinB, INPUT);
+	lastA = digitalRead(pinA);
+	lastB = digitalRead(pinB);
+	previous = encodeSequencePosition(lastA, lastB);
+}
+
+void encoderChanged() {
+	if (PCintPort::arduinoPin == pinA) {
+		lastA = PCintPort::pinState;
+	} else {
+		lastB = PCintPort::pinState;
+	}
+
+	int current = encodeSequencePosition(lastA, lastB);
+	int diff = current - previous; //Difference is the difference between the two
+	if (diff < -1) {
+		diff = diff + 4;
+	} else {
+		if (diff > 1) {
+			diff = diff - 4;
+		}
+	}
+
+	counter = counter + diff;
+	previous = current;
+}
+
+void setup() {
+	// setup code goes here
+	//Initialize serial
+	Serial.begin(115200);
+	// get first reading
+	setupEncoder();
+	nunchuk.begin();
+	// attach interrupt to the two pins
+	PCintPort::attachInterrupt(pinA, encoderChanged, CHANGE);
+	PCintPort::attachInterrupt(pinB, encoderChanged, CHANGE);
+}
+
+int stride = 0;
+void loop() {
+	// code that should be repeated goes here
+
+	float mapcounter = counter / 3600.0;
+	delay(250);
+	//Serial.println(mapcounter, DEC);
+	//Serial.println(counter, DEC);
+	nunchuk.report.joystickAxes[0] = lowByte(counter);
+        nunchuk.applyReportUpdates();
+	nunchuk.printReceiveData();
+
+        if (stride == 0) {
+           Serial.print("Memory dump: ");
+           for (uint8_t * loc = nunchuk.getMemoryLocation(0), * last = nunchuk.getMemoryLocation(0xff); loc <= last; ++loc) {
+            Serial.print(*loc, HEX);
+            Serial.print(" ");
+           }
+           Serial.println("");
+        }
+        stride = (stride+1)%20;	//Serial.println ();
+
+	//Serial Write
+	/*
+	Serial.write ("I =  ");
+	 Serial.println (Ival,DEC);
+	 Serial.write ("A =  ");
+	 Serial.println (Aval,DEC);
+	 Serial.write ("B =  ");
+	 Serial.println (Bval,DEC);
+	 Serial.println();
+	 delay(500);
+	 */
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Encoder/FakeWiimoteExtension.h
@@ -0,0 +1,125 @@
+/** @file
+	@brief Header
+
+	@date 2012
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+
+*/
+
+//           Copyright Iowa State University 2012.
+//  Distributed under the Boost Software License, Version 1.0.
+//     (See accompanying file LICENSE_1_0.txt or copy at
+//           http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+#ifndef INCLUDED_FakeWiimoteExtension_h_GUID_900138B8_E901_4FB9_FBD5_706A5C94195C
+#define INCLUDED_FakeWiimoteExtension_h_GUID_900138B8_E901_4FB9_FBD5_706A5C94195C
+
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+#include <stdint.h>
+#include <string.h> // for memcpy
+
+
+class FakeNunchuk {
+	public:
+		enum {
+			NunchukAddr = 0x52,
+			ReplyLength = 6,
+			HandshakeLength = 224, /* from wiiuse */
+			EncryptionEnabled = 0xAA,
+			EncryptionDisabled = 0x55
+		};
+
+		enum ExpansionRegisterAddress {
+			OutputData = 0x00,
+			IDRegister = 0xFA,
+			CalibrationRegister = 0x20,
+			EncryptionRegister = 0xF0,
+                        SecondaryInitRegister = 0xFB
+		};
+
+		struct DataReport {
+			uint8_t joystickAxes[2];
+			uint8_t accelAxesHigh[3];
+			unsigned int accelZLow : 2;
+			unsigned int accelYLow : 2;
+			unsigned int accelXLow : 2;
+			bool buttonC : 1;
+			bool buttonZ : 1;
+		};
+		struct CalibData {
+			uint8_t accelCalibZero[3];
+			uint8_t accelCalibG[3];
+			struct JSCalibAxis {
+				uint8_t maxVal;
+				uint8_t minVal;
+				uint8_t centerVal;
+			} jsCalibAxes[2];
+		};
+
+		FakeNunchuk();
+
+		void begin();
+
+		void printReceiveData();
+
+		uint8_t * getMemoryLocation(uint8_t loc) {
+			return &(memorySpace[loc]);
+		}
+
+		void applyReportUpdates(){
+	memcpy(getMemoryLocation(OutputData), &report, sizeof(report));
+}
+		DataReport report;
+
+	private:
+                bool initialized;
+                void wipeMemory();
+                void initializeMemory();
+		static const uint8_t expansionID[6];
+		static const CalibData calib;
+		static const uint8_t bufSize = 32;
+		uint8_t addr;
+		uint8_t bytes[bufSize];
+		uint8_t memorySpace[256];
+		uint8_t receivedBytes;
+		uint8_t gotReceiveEvent;
+		uint8_t gotRequestEvent;
+                uint8_t gotWriteData;
+
+		uint8_t addr_log[256];
+		void receiveEvent(int howMany);
+		void requestEvent();
+
+
+		uint8_t encryptByte(const uint8_t val) {
+			return (val - 0x17) ^ 0x17;
+		}
+
+		uint8_t decryptByte(const uint8_t val) {
+			return (val ^ 0x17) + 0x17;
+		}
+
+		bool encrypted;
+		bool sentID;
+		static FakeNunchuk * self;
+		static void _receiveTrampoline(int howMany);
+		static void _requestTrampoline();
+};
+
+
+#endif // INCLUDED_FakeWiimoteExtension_h_GUID_900138B8_E901_4FB9_FBD5_706A5C94195C
+
